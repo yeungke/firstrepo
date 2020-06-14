ce4709f58c2afeeda4391729d3429e5c5dd26ca2
yeungke (Kevin Yeung) <yeungke@sheridancollege.ca>
2020-06-14 13:37:55 -0400

    A very unsuccessful build of the first assignment. I'm struggling with understanding Javascript commands. I've been using YouTube tutorials and the textbook to help me understand, but I haven't made any progress. For now, I've spent too much time on this assignment; I'm just going to submit what I have and prepare for the next one (as well as preparing for other courses).

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,11 @@
+package studentlist;
+
+public class StudentList {
+    Student[] students = new Student[2];
+    
+     Scanner input = new Scanner(System.in);
+     
+     for(int i = 0; i < students.length; i++) {
+        System.out.println
+}
+}
